fa2b0069ebd25067cf8e4160df0c59ae75c59cf9
gets23 <ghaitsya.faradiba@gmail.com>
2025-06-14 01:05:31 +0700

    ruto

--- homepage.php
+++ homepage.php
@@ -135,7 +135,7 @@
       </script>
     </main>
     <!-- Footer Copyright -->
-    <footer class="border-t text-center text-sm text-gray-600 py-3 mt-8">
+    <footer class="border-t text-center text-sm text-gray-600 py-3">
       Copyright © <a href="https://jagosepeda.com" class="hover:underline text-blue-700">jagosepeda.com</a> 2025
     </footer>
   </div>
