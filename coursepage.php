6fcff64869f73ab909824220b148a12a342e06c8
gets23 <ghaitsya.faradiba@gmail.com>
2025-06-14 13:31:05 +0700

    night before the end

--- coursepage.php
+++ coursepage.php
@@ -115,19 +115,19 @@
                 const kursusContainer = document.getElementById("daftarKursus");
                 kursusList.forEach((item, index) => {
                     kursusContainer.innerHTML += `
-                        <div class="bg-gray-100 rounded-xl shadow px-4 py-3 flex flex-col justify-between h-[200px]">
+                        <div class="bg-gray-100 rounded-xl shadow px-4 py-3 flex flex-col justify-between h-[100px]">
                             <div class="flex gap-4 flex-1">
-                            <div class="w-1/4 bg-white aspect-[3/4] rounded-md flex items-center justify-center">
-                                <div class="bg-black text-white rounded-full text-xs px-2 py-1 font-semibold">#${index + 1}</div>
-                            </div>
-                            <div class="flex-1 flex flex-col justify-between">
-                                <div>
-                                <h3 class="font-semibold text-sm mb-1">${item.judul}</h3>
-                                <p class="text-xs text-gray-600 leading-snug mb-2">${item.deskripsi}</p>
-                                <p class="text-xs text-gray-500">${item.materi} Materi | ${item.kuis} Kuis</p>
+                                <div class="w-1/4 bg-white aspect-[3/4] rounded-md flex items-center justify-center">
+                                    <div class="bg-black text-white rounded-full text-xs px-2 py-1 font-semibold">#${index + 1}</div>
                                 </div>
-                                <button class="bg-blue-900 text-white text-sm px-4 py-1 rounded hover:bg-blue-800 transition mt-2 w-fit">Mulai</button>
-                            </div>
+                                <div class="flex-1 flex flex-col justify-between">
+                                    <div>
+                                    <h3 class="font-semibold text-sm mb-1">${item.judul}</h3>
+                                    <p class="text-xs text-gray-600 leading-snug mb-2">${item.deskripsi}</p>
+                                    <p class="text-xs text-gray-500">${item.materi} Materi | ${item.kuis} Kuis</p>
+                                    </div>
+                                    <button class="bg-blue-900 text-white text-sm px-4 py-1 rounded hover:bg-blue-800 transition mt-2 w-fit">Mulai</button>
+                                </div>
                             </div>
                         </div>
                     `;
