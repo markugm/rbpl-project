b2f1d0a254897ea4abe1a14e62752dc15b3f601c
gets23 <ghaitsya.faradiba@gmail.com>
2025-07-11 20:37:07 +0700

    nyangho

--- resultpage.php
+++ resultpage.php
@@ -2,7 +2,7 @@
 <html lang="id">
 <head>
   <meta charset="UTF-8">
-  <title>Jago Sepeda Dashboard</title>
+  <title>Jago Sepeda Result</title>
   <script src="https://cdn.tailwindcss.com"></script>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
 </head>
