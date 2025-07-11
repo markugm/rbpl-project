9d74f462b4e08f12cacaf8510164555e1e46d0a6
gets23 <ghaitsya.faradiba@gmail.com>
2025-07-11 17:24:22 +0700

    unag

--- reviewpage.php
+++ reviewpage.php
@@ -39,6 +39,7 @@ $jawaban = [
   <meta charset="UTF-8">
   <title>Review Kuis - <?= htmlspecialchars($judulKuis) ?></title>
   <script src="https://cdn.tailwindcss.com"></script>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
 </head>
 <body class="bg-gray-100 font-sans">
   <div class="flex min-h-screen">
