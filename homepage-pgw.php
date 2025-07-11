aead702a16d2445982a43d4b0810149e0d0549fa
gets23 <ghaitsya.faradiba@gmail.com>
2025-07-12 03:50:23 +0700

    corydoras

--- homepage-pgw.php
+++ homepage-pgw.php
@@ -39,7 +39,7 @@ if (!isset($_SESSION['logged_in']) || $_SESSION['role'] !== 'pegawai') {
         <a href="#" class="flex items-center gap-2 px-4 py-2 hover:bg-white/10 rounded">
           <i class="bi bi-gear"></i> Pengaturan
         </a>
-        <a href="#" class="flex items-center gap-2 px-4 py-2 hover:bg-white/10 rounded">
+        <a href="logoutpage.php" class="flex items-center gap-2 px-4 py-2 hover:bg-white/10 rounded">
           <i class="bi bi-box-arrow-right"></i> Keluar
         </a>
       </div>
