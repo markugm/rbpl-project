e61d7f435f097e853b8eeb5ed529ab261586c6d0
gets23 <ghaitsya.faradiba@gmail.com>
2025-07-12 03:49:44 +0700

    set net g0

--- homepage-pgw.php
+++ homepage-pgw.php
@@ -1,3 +1,11 @@
+<?php
+session_start();
+if (!isset($_SESSION['logged_in']) || $_SESSION['role'] !== 'pegawai') {
+    header("Location: loginpage.php");
+    exit();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="id">
 <head>
